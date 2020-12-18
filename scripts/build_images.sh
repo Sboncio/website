4a54f9bfff0599dd81c2c4c65e0b9800c275b80f
Ubuntu <ubuntu@ip-172-31-41-230.eu-west-1.compute.internal>
2020-12-18 14:32:17 +0000

    repairs to docker scripts

--- scripts/build_images.sh
+++ scripts/build_images.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+export DOCKER_USERNAME=sboncio
+export DOCKER_PASSWORD=Hannahandsam1
 
 sudo docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+cd app
+echo $(pwd)
+sudo docker build . --tag $DOCKER_USERNAME/app:latest
 
-sudo docker build app $DOCKER_USERNAME/app:latest
 
 sudo docker push $DOCKER_USERNAME/app:latest
