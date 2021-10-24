7e1ea652cc17e7f395f068ffc3e4a3ecc016d8c0
sam <samuelasquith@btinternet.com>
2021-10-25 00:06:04 +0100

    changes

--- scripts/deploy_images.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo $MYSQL_PASSWORD | docker secret create MYSQL_ROOT_PASSWORD - 
-sudo docker-compose down
-sudo docker pull $DOCKER_USERNAME/app:latest
-sudo docker-compose up -d
--- scripts/test_software.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sudo apt install python3 python3-pip python3-pytest -y
-pip3 install -r app/requirements.txt
-cd app
-python3 -m pytest
